8a60837f0d0a642e0c545471016710d43e435d20
unmono <dieselnt@gmail.com>
2023-01-22 02:47:58 +0100

    Change _validate_structure to mapping all sheets at once.

--- src/parsebp/bosch_price.py
+++ src/parsebp/bosch_price.py
@@ -1,7 +1,23 @@
+import re
 from functools import cached_property
 from openpyxl import Workbook
+from typing import Tuple, Protocol, ClassVar
 
-from bp_settings import PriceList
+from parsebp.models import (
+    PriceList,
+    MasterData,
+    NewRelease,
+    Discontinued,
+    References,
+)
+
+
+class UnsupportedFileStructureError(Exception):
+    pass
+
+
+class BPModel(Protocol):
+    sheet_pattern: ClassVar[re.Pattern]
 
 
 class BoschPrice:
@@ -11,14 +27,34 @@ class BoschPrice:
 
     def __init__(self, wb: Workbook):
         self.wb = wb
+        self.sheet_names = wb.sheetnames
 
-    @cached_property
-    def _validate_structure(self) -> int | None:
+        self.required_model = PriceList
+        self.extra_models = [
+            MasterData,
+            NewRelease,
+            Discontinued,
+            References,
+        ]
+
+    def map_model_to_sheet(self, model: BPModel) -> Tuple[str, BPModel] | None:
         """
-        Finds out if there is a pricelist sheet in loaded file.
-        :return: index of pricelist sheet
+        Searches in sheet names of the loaded file for patterns of models
+        :param model: bp model
+        :return: Tuple('Corresponding sheet name', bp model)
         """
-        for i, sheet in enumerate(self.wb.sheetnames):
-            if PriceList.sheet_pattern.search(sheet):
-                return i
+        for sheet in self.sheet_names:
+            if model.sheet_pattern.search(sheet):
+                return sheet, model
         return None
+
+    def mapped_sheets(self):
+        if (required_sheet := self.map_model_to_sheet(self.required_model)) is None:
+            raise UnsupportedFileStructureError('Unsupported file structure')
+
+        extra_sheets = [
+            t for model in self.extra_models
+            if (t := self.map_model_to_sheet(model)) is not None
+        ]
+
+        return [required_sheet, ] + extra_sheets
