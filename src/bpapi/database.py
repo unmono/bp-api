cb6b7173fa11fd13d3a58bb574257e8177e310fb
unmono <dieselnt@gmail.com>
2023-02-19 00:23:41 +0100

    Database connection and set of needed models.

---
+++ src/bpapi/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = "sqlite:///../bp.sql"
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={'check_same_thread': False},
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
---
+++ src/bpapi/models.py
@@ -0,0 +1,122 @@
+from decimal import Decimal
+from typing import List
+from typing_extensions import Annotated
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+    Boolean,
+    DECIMAL,
+)
+from sqlalchemy.orm import (
+    relationship,
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    MappedAsDataclass,
+)
+
+
+class Base(DeclarativeBase, MappedAsDataclass):
+    pass
+
+
+id_pk = Annotated[int, mapped_column('id', Integer, init=False, primary_key=True)]
+rowid_pk = Annotated[int, mapped_column('rowid', Integer, init=False, primary_key=True)]
+partnum_fk = Annotated[int, mapped_column(ForeignKey('partnum.id'))]
+
+
+class Section(Base):
+    """
+    Section of Bosch catalogue hierarchy.
+    """
+    __tablename__ = 'sect'
+
+    id: Mapped[id_pk]
+    title: Mapped[str]
+    subsections: Mapped[List['SubSection']] = relationship(back_populates='section')
+
+
+class SubSection(Base):
+    """
+    Subsection of Bosch catalogue hierarchy. Refers to Section.
+    """
+    __tablename__ = 'subsect'
+
+    id: Mapped[id_pk]
+    title: Mapped[str]
+    sect_id: Mapped[int] = mapped_column(ForeignKey('sect.id'))
+    section: Mapped['Section'] = relationship(back_populates='subsections')
+    subsubsections: Mapped['Subsub'] = relationship(back_populates='subsection')
+
+
+class Subsub(Base):
+    """
+    Subsubsection of Bosch catalogue heirarchy. Referes to Subsection.
+    """
+    __tablename__ = 'subsub'
+
+    id: Mapped[id_pk]
+    title: Mapped[str]
+    subsect_id: Mapped[int] = mapped_column(ForeignKey('subsect.id'))
+    subsubsections: Mapped['SubSection'] = relationship(back_populates='subsubsection')
+
+
+class PartNumber(Base):
+    """
+    partnum table entry. Represents every patnum occured in Bosch price, whether
+    in price itself or in new release, discontinued or reference sections.
+    Stored along with flags - discontinued or new release. And refers to self
+    to bound similar products.
+    """
+    __tablename__ = 'partnum'
+
+    id: Mapped[rowid_pk]
+    part_no: Mapped[str]
+    discontinued: Mapped[bool]
+    new_release: Mapped[bool]
+    products: Mapped['Product'] = relationship(back_populates='partnumber')  # Same name as field? Without name?
+    masterdata: Mapped['MasterData'] = relationship(back_populates='partnumber')
+
+
+class Product(Base):
+    """
+    Bosch product in pricelist. Stores descriptions, references on sections, price
+    and some product related parameters. References on particular partnum entry.
+    """
+    __tablename__ = 'pricelist'
+
+    title_ua: Mapped[str]
+    title_en: Mapped[str]
+    uktzed: Mapped[int]
+    min_order: Mapped[int]
+    quantity: Mapped[int]
+    price: Mapped[DECIMAL]
+    truck: Mapped[bool]
+    id: Mapped[rowid_pk]
+    partnum_id: Mapped[partnum_fk]
+    partnum: Mapped['PartNumber'] = relationship()
+    subsub_id: Mapped[int] = mapped_column(ForeignKey('subsub.id'))
+    subsub: Mapped['PartNumber'] = relationship(back_populates='products')
+
+
+class MasterData(Base):
+    """
+    Physical pararmeters of product. Weigth, dimensions, etc.
+    """
+    __tablename__ = 'masterdata'
+
+    ean: Mapped[int]
+    gross: Mapped[DECIMAL]
+    net: Mapped[DECIMAL]
+    weight_unit: Mapped[str]
+    length: Mapped[int]
+    width: Mapped[int]
+    height: Mapped[int]
+    measure_unit: Mapped[str]
+    volume: Mapped[DECIMAL]
+    volume_unit: Mapped[str]
+    id: Mapped[rowid_pk]
+    partnum_id: Mapped[partnum_fk]
+    partnum: Mapped['PartNumber'] = relationship(back_populates='masterdata')
