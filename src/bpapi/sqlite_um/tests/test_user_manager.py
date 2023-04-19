405fd2ee69a5fe9f306c8a8aaaf823e7f9465489
unmono <dieselnt@gmail.com>
2023-04-19 23:34:40 +0200

    User manager class and some tests for it.

---
+++ src/bpapi/sqlite_um/tests/test_user_manager.py
@@ -0,0 +1,109 @@
+import pytest
+import sqlite3
+import os
+from ..user_manager import SQLiteUserManager, UserAlreadyExists, UserDoesNotExist
+
+
+EXAMPLE_USER = {
+    'username': 'example_user',
+    'password': 'password',
+    'scopes': ['scope1', 'scope2']
+}
+
+
+SUPPORTED_TABLE_SCHEMAS = [
+    {
+        'username': 'TEXT',
+        'password': 'TEXT',
+        'scopes': 'TEXT',
+    },
+    {},
+]
+
+UNSUPPORTED_TABLE_SCHEMAS = [
+    {
+        'username': 'TEXT',
+        'scopes': 'TEXT',
+    },
+    {
+        'username': 'TEXT',
+        'password': 'TEXT',
+        'scopes': 'TEXT',
+        'not_expected': 'TEXT',
+    },
+]
+
+
+@pytest.fixture(params=SUPPORTED_TABLE_SCHEMAS)
+def given_db(request):
+    db_name = 'normal.sqlite'
+    if request.param:
+        conn = sqlite3.connect(db_name)
+        conn.execute(f"""
+            CREATE TABLE users ({','.join([f'{k} {v}' for k, v in request.param.items()])})
+        """)
+        conn.close()
+    yield SQLiteUserManager(db_name)
+    os.remove(db_name)
+
+
+@pytest.fixture
+def setted_up_db(given_db):
+    given_db._initial_setup()
+    return given_db
+
+
+def test_initial_setup(given_db):
+    try:
+        given_db._initial_setup()
+    except AssertionError as e:
+        assert False, repr(e)
+
+
+@pytest.mark.parametrize('given_db', UNSUPPORTED_TABLE_SCHEMAS, indirect=True)
+def test_initial_setup_unsupported_db(given_db):
+    with pytest.raises(AssertionError):
+        given_db._initial_setup()
+
+
+def test_add_user(setted_up_db):
+    assert EXAMPLE_USER['username'] not in setted_up_db
+    setted_up_db.add_user(**EXAMPLE_USER)
+    with setted_up_db._db_connection() as db:
+        just_added_user = db.execute(
+            'SELECT COUNT(*) FROM users WHERE username = ?;',
+            (EXAMPLE_USER['username'], )
+        ).fetchone()
+
+    assert just_added_user[0] == 1
+
+
+def test_add_existed_user(setted_up_db):
+    setted_up_db.add_user(**EXAMPLE_USER)
+    with pytest.raises(UserAlreadyExists):
+        setted_up_db.add_user(**EXAMPLE_USER)
+
+
+def test_delete_user(setted_up_db):
+    setted_up_db.add_user(**EXAMPLE_USER)
+    setted_up_db.delete_user(EXAMPLE_USER['username'])
+    with setted_up_db._db_connection() as db:
+        just_added_user = db.execute(
+            'SELECT COUNT(*) FROM users WHERE username = ?;',
+            (EXAMPLE_USER['username'], ),
+        ).fetchone()
+
+    assert just_added_user[0] == 0
+
+
+def test_get_user_dict(setted_up_db):
+    setted_up_db.add_user(**EXAMPLE_USER)
+    user_dict = setted_up_db.get_user_dict(EXAMPLE_USER['username'])
+
+    assert user_dict == EXAMPLE_USER
+
+
+def test_get_not_existed_user_dict(setted_up_db):
+    setted_up_db.add_user(**EXAMPLE_USER)
+    with pytest.raises(UserDoesNotExist):
+        user_dict = setted_up_db.get_user_dict('not_existed_username')
---
+++ src/bpapi/sqlite_um/user_manager.py
@@ -0,0 +1,113 @@
+import contextlib
+import os
+import sqlite3
+from typing import Generator
+
+
+class UserAlreadyExists(Exception):
+    pass
+
+
+class UserDoesNotExist(Exception):
+    pass
+
+
+class DatabaseCorrupted(Exception):
+    pass
+
+
+class SQLiteUserManager:
+    """
+    Defines user management methods.
+    Provides 'in' usage.
+    """
+
+    def __init__(
+        self,
+        database_path: str | bytes | os.PathLike,
+        table_name: str | None = 'users',
+    ):
+        # todo? kwargs user schema
+        self.database_path = database_path
+        self.table_name = table_name
+
+    def __contains__(self, username: str) -> bool:
+        with self._db_connection() as db:
+            user_count = db.execute(
+                f'SELECT COUNT(*) FROM {self.table_name} WHERE username=?;',
+                (username, )
+            ).fetchone()[0]
+        assert user_count in (0, 1), 'Whoa, something crazy with the database!'
+        return bool(user_count)
+
+    def add_user(
+        self,
+        username: str,
+        password: str,
+        scopes: list[str],
+    ) -> None:
+        if username in self:
+            raise UserAlreadyExists('This username is already used.')
+        data_to_insert = {
+            'username': username,
+            'password': password,
+            'scopes': ','.join(scopes)
+        }
+        with self._db_connection() as db:
+            with db:
+                db.execute(f"""
+                    INSERT INTO {self.table_name} 
+                    VALUES (:username, :password, :scopes);
+                """, data_to_insert)
+
+    def delete_user(self, username: str) -> None:
+        with self._db_connection() as db:
+            with db:
+                db.execute(f"""
+                    DELETE FROM {self.table_name}
+                    WHERE username = ?;
+                """, (username, ))
+
+    def get_user_dict(self, username: str) -> dict[str, str | list[str]] | None:
+        with self._db_connection() as db:
+            values = db.execute(f"""
+                SELECT username, password, scopes
+                FROM {self.table_name}
+                WHERE username = ?
+            """, (username, )).fetchone()
+        if values:
+            keys = ('username', 'password', 'scopes')
+            user_dict = dict(zip(keys, values))
+            user_dict['scopes'] = user_dict['scopes'].split(',')
+            return user_dict
+
+        raise UserDoesNotExist('User with this username doesn\'t exist.')
+
+    def _initial_setup(self) -> None:
+        expected_table_structure = [
+            ('username', 'TEXT'),
+            ('password', 'TEXT'),
+            ('scopes', 'TEXT'),
+        ]
+        with self._db_connection() as db:
+            db.execute(f"""
+                CREATE TABLE IF NOT EXISTS {self.table_name} (
+                    {','.join([' '.join(field_tuple) for field_tuple in expected_table_structure])}            
+                );
+            """)
+
+            # todo? separate method
+            results = db.execute("""
+                SELECT name, type 
+                FROM pragma_table_info(?);
+            """, (self.table_name, )).fetchall()
+
+            assert results == expected_table_structure, 'Table structure is not supported.'
+
+    @contextlib.contextmanager
+    def _db_connection(self) -> Generator[sqlite3.Connection, None, None]:
+        conn = sqlite3.connect(self.database_path)
+        try:
+            yield conn
+        finally:
+            conn.close()
