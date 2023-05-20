8dc7c17b44ba0e517f6abe2dc0f1e045c5c318fa
unmono <dieselnt@gmail.com>
2023-05-21 01:09:55 +0200

    Add /me endpoint for logged user

--- src/bpapi/routers/login.py
+++ src/bpapi/routers/login.py
@@ -1,21 +1,22 @@
 from typing import Annotated
 from datetime import timedelta
 
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, Security
 from fastapi.security import OAuth2PasswordRequestForm
 
-from schemas import Token
-from dependencies import authenticate_user, create_token
+from schemas import Token, User
+from dependencies import authenticate_user, create_token, get_current_user
 
 from settings import ApiSettings
 settings = ApiSettings()
 
 router = APIRouter(
     tags=['Auth'],
+    prefix=settings.ROUTE_PREFIX + '/login',
 )
 
 
-@router.post('/api/v1/login/', response_model=Token)
+@router.post('/', response_model=Token)
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user = authenticate_user(form_data.username, form_data.password)
     token_expire_delta = timedelta(hours=settings.TOKEN_EXPIRE_HOURS)
@@ -24,3 +25,15 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
         expires_delta=token_expire_delta,
     )
     return Token(access_token=token, token_type='bearer')
+
+
+@router.get(
+    '/me',
+    response_model=User,
+    tags=['User', ],
+)
+def get_logged_user(user: Annotated[
+    User,
+    Security(get_current_user, scopes=['catalogue'])
+]):
+    return user
