a1698e4b4f1b1b6a5ae9a1f20473890d5e0b2005
unmono <dieselnt@gmail.com>
2023-05-30 22:23:26 +0200

    Move utlis and frontend to separate repos. Change src to api. Ductape.

--- src/frontend/src/components/PlusMinus.tsx
+++
@@ -1,8 +0,0 @@
-type flagType = {
-  sw: boolean
-}
-
-export default function (props: flagType) {
-  const {sw} = props;
-  return <span style={{fontFamily: 'Courier'}}>{sw ? '-': '+'}</span>
-}
--- src/frontend/src/functions.ts
+++
@@ -1,4 +0,0 @@
-export const validatePartnum = (value: string) => {
-  const re = new RegExp(/^[A-Z0-9]{10}$/);
-  return re.test(value);
-}
